fb78e63be59e1805db063540415af0fda3ef6837
Sadman Ahmed <sadman.sakif.ahmed@gmail.com>
2023-03-12 11:59:54 -0400

    added Holiday page

---
+++ src/main/java/com/sadman/School_management/model/Holiday.java
@@ -0,0 +1,15 @@
+package com.sadman.School_management.model;
+
+import lombok.Data;
+
+@Data
+public class Holiday {
+
+    private final String day;
+    private final String reason;
+    private final Type type;
+
+    public enum Type {
+        FESTIVAL, FEDERAL
+    }
+}
