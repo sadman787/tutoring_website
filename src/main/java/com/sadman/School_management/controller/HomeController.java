bd0c947cdcfb6b699c377e0a4aadf8600e98a830
Sadman Ahmed <sadman.sakif.ahmed@gmail.com>
2023-03-12 11:40:00 -0400

    home page of school/tutoring service management website

---
+++ src/main/java/com/sadman/School_management/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.sadman.School_management.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = {"", "/", "home"})
+    public String displayHomePage(){
+        return "home.html";
+    }
+}
