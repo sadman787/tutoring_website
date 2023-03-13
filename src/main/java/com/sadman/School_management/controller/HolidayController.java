f6b946549654eda80dfe4ff0f9e95292beb30aa5
Sadman Ahmed <sadman.sakif.ahmed@gmail.com>
2023-03-12 23:46:06 -0400

    added path variable in holiday endpoint

--- src/main/java/com/sadman/School_management/controller/HolidayController.java
+++ src/main/java/com/sadman/School_management/controller/HolidayController.java
@@ -4,6 +4,8 @@ import com.sadman.School_management.model.Holiday;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,8 +14,17 @@ import java.util.stream.Collectors;
 @Controller
 public class HolidayController {
 
-    @GetMapping("/holidays")
-    public String displayHolidays(Model model) {
+    @GetMapping("/holidays/{display}")
+    public String displayHolidays(@PathVariable String display, Model model) {
+        if (display != null && display.equals("all")){
+            model.addAttribute("festival", true);
+            model.addAttribute("federal", true);
+        }
+        else if (display != null && display.equals("festival")){
+            model.addAttribute("festival", true);
+        } else if (display != null && display.equals("federal")) {
+            model.addAttribute("federal", true);
+        }
         List<Holiday> holidays = Arrays.asList(
                 new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
                 new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
