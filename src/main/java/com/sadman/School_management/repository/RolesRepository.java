6b488dd7271c93294242ee70dd7a1f853ba32dfc
Sadman Ahmed <sadman.sakif.ahmed@gmail.com>
2023-03-19 23:42:38 -0400

    Created repository and table for Person, Roles and Address

---
+++ src/main/java/com/sadman/School_management/repository/RolesRepository.java
@@ -0,0 +1,9 @@
+package com.sadman.school_management.repository;
+
+import com.sadman.school_management.model.Roles;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RolesRepository extends JpaRepository<Roles, Integer> {
+}
