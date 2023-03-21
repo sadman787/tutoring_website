ebded86c95b9bb3208aaf533ebc9fc5d80c23480
Sadman Ahmed <sadman.sakif.ahmed@gmail.com>
2023-03-21 01:08:24 -0400

    Created Profile page and populated information from database

--- src/main/java/com/sadman/school_management/controller/DashboardController.java
+++ src/main/java/com/sadman/school_management/controller/DashboardController.java
@@ -1,6 +1,10 @@
 package com.sadman.school_management.controller;
 
+import com.sadman.school_management.model.Person;
+import com.sadman.school_management.repository.PersonRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,10 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 @Controller
 public class DashboardController {
+
+    @Autowired
+    PersonRepository personRepository;
     @RequestMapping("/dashboard")
-    public String displayDashboard(Model model, Authentication authentication){
-        model.addAttribute("username", authentication.getName());
+    public String displayDashboard(Model model, Authentication authentication, HttpSession httpSession){
+        Person person = personRepository.readByEmail(authentication.getName());
+        model.addAttribute("username", person.getName());
         model.addAttribute("roles", authentication.getAuthorities().toString());
+        httpSession.setAttribute("loggedInPerson", person);
         return "dashboard.html";
     }
 }
