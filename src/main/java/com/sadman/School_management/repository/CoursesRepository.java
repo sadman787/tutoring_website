67f7e6e8d923edffd9031065ef0bca3b333a294e
Sadman Ahmed <sadman.sakif.ahmed@gmail.com>
2023-03-26 02:31:19 -0400

    Pagination and sorting implemented for open messages for admin

--- src/main/java/com/sadman/School_management/repository/CoursesRepository.java
+++ src/main/java/com/sadman/School_management/repository/CoursesRepository.java
@@ -4,6 +4,11 @@ import com.sadman.school_management.model.Courses;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CoursesRepository extends JpaRepository<Courses, Integer> {
+    List<Courses> findByOrderByNameDesc();
+
+    List<Courses> findByOrderByName();
 }
--- src/main/java/com/sadman/school_management/repository/ContactRepository.java
+++ src/main/java/com/sadman/school_management/repository/ContactRepository.java
@@ -1,13 +1,16 @@
 package com.sadman.school_management.repository;
 
 import com.sadman.school_management.model.Contact;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ContactRepository extends CrudRepository<Contact, Integer> {
-
+public interface ContactRepository extends PagingAndSortingRepository<Contact, Integer>, CrudRepository<Contact, Integer>  {
     List<Contact> findByStatus(String status);
 
+    Page<Contact> findByStatus(String status, Pageable pageable);
 }
